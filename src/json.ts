9b7fbd8ebe09054ab40f824c6d7cc597cfa2bb23
NyaHub <yaya70038@gmail.com>
2026-05-10 23:08:34 +0500

    fix license

--- src/json.ts
+++ src/json.ts
@@ -2,7 +2,7 @@
  * @nyahub/jsonsafe
  * Copyright (C) 2026 nyahub
  * * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation...
  */
 
